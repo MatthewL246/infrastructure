c32d0db0c2ae269bde674cc77ee8b9c035bcd0cf
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2025-12-17 18:36:27 -0500

    Reformat variable descriptions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "hetzner_cloud_token" {
-  description = "Token for the Hetzner Cloud project (read/write)"
+  description = "Token for the Hetzner Cloud project (read/write)."
   type        = string
   sensitive   = true
 }
 
 variable "hetzner_ssh_keys" {
-  description = "SSH keys for Hetzner servers, which will be authorized on all servers. Key is name (user@hostname), value is public key (ssh-ed25519 ...)"
+  description = "SSH keys for Hetzner servers, which will be authorized on all servers. Key is name (user@hostname), value is public key (ssh-ed25519 ...)."
   type        = map(string)
 }
 
@@ -16,13 +16,13 @@ variable "hetz_de_ssh_port_changed" {
 }
 
 variable "cloudflare_api_token" {
-  description = "Cloudflare account API token (required single-zone permissions: DNS:Edit)"
+  description = "Cloudflare account API token (required single-zone permissions: DNS:Edit)."
   type        = string
   sensitive   = true
 }
 
 variable "cloudflare_zone_id" {
-  description = "Cloudflare Zone ID for the website (must match the one authorized by the API token)"
+  description = "Cloudflare Zone ID for the website (must match the one authorized by the API token)."
   type        = string
   sensitive   = true
 }
