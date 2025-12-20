bab603305ab7e1a5c85fed659890b4a62a558e9e
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2025-12-19 21:07:03 -0500

    Add note about the Ansible Terraform provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,10 @@ terraform {
     }
   }
 }
+
+# I tried using the Ansible Terraform provider (https://github.com/ansible/terraform-provider-ansible) but found it
+# half-baked and annoying to use
+# - You can only view a playbook's output after it has completely finished
+# - The ansible_inventory resource is useless because you can't actually use it with an ansible_playbook resource:
+#   https://github.com/ansible/terraform-provider-ansible/issues/37
+# - Development seems to have stalled since 2024
