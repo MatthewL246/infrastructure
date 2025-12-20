c95cb51d11a2c0faab3fde3411016c210cd28178
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2025-12-19 19:46:48 -0500

    Switch server IP address source to hcloud_server

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -6,7 +6,7 @@ resource "cloudflare_dns_record" "terraform_test_ipv4" {
   zone_id = var.cloudflare_zone_id
   name    = "terraform_test"
   type    = "A"
-  content = hcloud_primary_ip.hetz_de_ipv4.ip_address
+  content = hcloud_server.hetz_de.ipv4_address
   ttl     = 1
   proxied = false
   comment = "Terraform test record (IPv4)"
@@ -16,7 +16,7 @@ resource "cloudflare_dns_record" "terraform_test_ipv6" {
   zone_id = var.cloudflare_zone_id
   name    = "terraform_test"
   type    = "AAAA"
-  content = cidrhost(hcloud_primary_ip.hetz_de_ipv6.ip_network, 1)
+  content = hcloud_server.hetz_de.ipv6_address
   ttl     = 1
   proxied = false
   comment = "Terraform test record (IPv6)"
