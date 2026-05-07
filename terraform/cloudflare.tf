8d9255bae51df52981a8432f5c5233c48e1c8d94
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2026-05-06 20:02:27 -0400

    Make Cloudflare DNS record names fully-qualified
    It was working with just subdomains, but this resulted in references to the DNS record's name giving different results between the first application (where it was just the provided subdomain) it and subsequent ones (where it turned into a FQDN).

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -8,7 +8,7 @@ data "cloudflare_zone" "primary_domain" {
 
 resource "cloudflare_dns_record" "gateway_ipv4" {
   zone_id = var.cloudflare_zone_id
-  name    = "gateway-staging"
+  name    = "gateway-staging.${data.cloudflare_zone.primary_domain.name}"
   type    = "A"
   content = hcloud_server.gateway.ipv4_address
   ttl     = 1
@@ -17,7 +17,7 @@ resource "cloudflare_dns_record" "gateway_ipv4" {
 
 resource "cloudflare_dns_record" "gateway_ipv6" {
   zone_id = var.cloudflare_zone_id
-  name    = "gateway-staging"
+  name    = "gateway-staging.${data.cloudflare_zone.primary_domain.name}"
   type    = "AAAA"
   content = hcloud_server.gateway.ipv6_address
   ttl     = 1
@@ -26,7 +26,7 @@ resource "cloudflare_dns_record" "gateway_ipv6" {
 
 resource "cloudflare_dns_record" "headscale_ipv4" {
   zone_id = var.cloudflare_zone_id
-  name    = "headscale-staging"
+  name    = "headscale-staging.${data.cloudflare_zone.primary_domain.name}"
   type    = "A"
   content = hcloud_server.gateway.ipv4_address
   ttl     = 1
@@ -35,7 +35,7 @@ resource "cloudflare_dns_record" "headscale_ipv4" {
 
 resource "cloudflare_dns_record" "headscale_ipv6" {
   zone_id = var.cloudflare_zone_id
-  name    = "headscale-staging"
+  name    = "headscale-staging.${data.cloudflare_zone.primary_domain.name}"
   type    = "AAAA"
   content = hcloud_server.gateway.ipv6_address
   ttl     = 1
